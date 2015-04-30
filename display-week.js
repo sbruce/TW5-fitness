e5a5e399acb73035c353b856a62c3b6ab3951e5b
Shaun Bruce <shaun.bruce@seagate.com>
2015-04-30 14:08:37 -0600

    Updated displayWeek macro to find show the current week starting with Sunday. Updated Dashboard to display the current week

--- display-week.js
+++ display-week.js
@@ -13,32 +13,65 @@ Display the workouts for the next seven days
 	exports.name = "displayWeek";
 
 	exports.params = [
-		{name: "year"},
-		{name: "month"},
-		{name: "day"},
+		{name: "dateString"},
 		{name: "template", default: "$:/plugins/sbruce/fitness/calendar-template"}
 	];
 
 	var nextDay = function(date) {
-		var tomorrow = new Date(date.getYear(), date.getMonth(), date.getDate());
+		var tomorrow = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 		tomorrow.setDate(tomorrow.getDate() + 1);
 		return tomorrow;
 	};
 
+	var splitDate = function(dateString) {
+		var year = dateString.substr(0,4);
+		var month = dateString.substr(4,2);
+		var day = dateString.substr(6,2);
+		return [year, month, day];
+	}
 
-	exports.run = function(year, month, day, template) {
+	var findSunday = function(date) {
+		var sunday = new Date(date);
+		for (var i = 0; i < date.getDay(); i++) {
+			sunday.setDate(sunday.getDate() - 1);
+		}
+		return sunday;
+	}
+
+	var dateToDateString = function(dateInput) {
+		var month = dateInput.getMonth() + 1;
+		if (month < 10) {
+			month = "0" + month;
+		}
+		var date = dateInput.getDate();
+		if (date < 10) {
+			date = "0" + date;
+		}
+		return dateInput.getFullYear() + month + date;
+	}
+
+	exports.run = function(dateString, template) {
+		if (dateString.length < 8) {
+			return;
+		}
+		var parsedDate = splitDate(dateString);
+		var year = parsedDate[0];
+		var month = parsedDate[1];
+		var day = parsedDate[2];
 		var result = "|";
-		var today = new Date(year, month, day);
+		var today = new Date(year + "/" + month + "/" + day);
+		var sunday = findSunday(today);
+		var curDay = new Date(sunday);
 		// Display the header
 		for (var i = 0; i < 7; i++) {
-			result = result + today.getMonth() + "/" + today.getDate() + "|";
-			today = nextDay(today);
+			result = result + curDay.getMonth() + "/" + curDay.getDate() + "|";
+			curDay = nextDay(curDay);
 		}
 		result = result + "\n|";
-		var today = new Date(year, month, day);
+		var curDay = new Date(sunday)
 		for (var i = 0; i < 7; i++) {
-			result = result + "<$list filter=\"[tag[Workout]year[" + year + "]day[" + today.getDate() + "]]\">{{||" + template + "}}</$list>|";
-			today = nextDay(today);
+			result = result + "<$list filter='[workout_date[" + dateToDateString(curDay) + "]]'>{{||" + template + "}}</$list>|";
+			curDay = nextDay(curDay);
 		}
 
 		result = result + "\n";
